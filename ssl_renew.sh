af538830b88e8f246240424741fe093028a777f2
Jeyson Ventura <jeysonv.sis@gmail.com>
2020-08-29 01:56:12 +0200

    fix plugins

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COMPOSE="/usr/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
+uOCKER="/usr/bin/docker"
 
 cd /home/docker-images/docker-wordpress/
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
