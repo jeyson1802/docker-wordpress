4ff97e22dff0fc53b0953390fa66de8f3c5e7d5e
Jeyson Junior Ventura Aguilar <jeysonv.sis@gmail.com>
2020-08-28 22:06:31 -0500

    ambiente desarrollo probado

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -79,12 +79,6 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
-}
-
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
